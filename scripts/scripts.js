9796476e2072cc599e04b2461099ee694d62a05d
blofft1 <93230607+blofft1@users.noreply.github.com>
2026-02-20 20:37:31 -0800

    Remove loadDa function and its invocation
    Removed deprecated loadDa function for loading daPreview script.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -338,9 +338,4 @@ async function loadPage() {
   loadDelayed();
 }
 
-loadPage()
-  (async function loadDa() {
-  if (!new URL(window.location.href).searchParams.get('dapreview')) return;
-  // eslint-disable-next-line import/no-unresolved
-  import('https://da.live/scripts/dapreview.js').then(({ default: daPreview }) => daPreview(loadPage));
-}());
+loadPage();

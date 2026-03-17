fd6b98c1913d26f48552ee32b728aa2f12c1c642
blofft1 <93230607+blofft1@users.noreply.github.com>
2026-03-16 22:30:36 -0700

    Fix loadPage call placement in scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -339,8 +339,9 @@ async function loadPage() {
   await loadLazy(document);
   loadDelayed();
 }
+
+loadPage();
 (async function loadDa() {
   if (!new URL(window.location.href).searchParams.get('dapreview')) return;
   import('https://da.live/scripts/dapreview.js').then(({ default: daPreview }) => daPreview(loadPage));
 }());
-loadPage();
